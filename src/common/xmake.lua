00c16c238b3b69c98e952b7c9119033dd07bb267
Wang <mwang740@gatech.edu>
2025-10-27 13:23:11 -0400

    disable fmt lib

--- src/common/xmake.lua
+++ src/common/xmake.lua
@@ -1,6 +1,6 @@
 set_languages("c++17")
 
-add_requires("fmt =8.1.1")
+add_requires("fmt =8.1.1", {configs = {header_only = true}})
 add_requires("nlohmann_json >=3.12.0")
 add_requires("eigen >=3.4.0")
 
@@ -10,6 +10,6 @@ target("common")
     add_headerfiles("*.h", "physbam/*.h")
     add_files("*.cpp")
     add_includedirs(".","physbam",{public=true})
-    add_packages("fmt",{configs = {header_only = true}, public=true})
+    add_packages("fmt",{public=true})
     add_packages("nlohmann_json",{public=true})
     add_packages("eigen",{public=true})
